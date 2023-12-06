5b3672fb641e4eec6a494f76b88dfe1dab21fa9c
francisjbae <150281972+francisjbae@users.noreply.github.com>
2023-12-06 11:23:11 -0800

    Add files via upload

---
+++ Engine.cpp
@@ -0,0 +1,76 @@
+#include "Engine.h"
+
+Engine::Engine()
+{
+    m_Window.create(VideoMode::getDesktopMode(), "Particles", Style::Default);
+}
+
+void Engine::run()
+{
+    Clock c;
+    cout << "Starting Particle unit tests..." << endl;
+    Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
+    p.unitTests();
+    cout << "Unit tests complete.  Starting engine..." << endl;
+    while (m_Window.isOpen())
+    {
+        Time dt = c.restart();
+        float dtAsSeconds = dt.asSeconds();
+        
+        input();
+        update(dtAsSeconds);
+        draw();
+    }
+}
+
+void Engine::input()
+{
+    Event event;
+    while (m_Window.pollEvent(event))
+    {
+        if (event.type == Event::Closed)
+        {
+            m_Window.close();
+        }
+        if (event.type == Event::MouseButtonPressed)
+        {
+            if (event.mouseButton.button == Mouse::Left)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Vector2i pos(event.mouseButton.x, event.mouseButton.y);
+                    int numPoints = rand() % 26 + 25;
+                    Particle p(m_Window, numPoints, pos);
+                    m_particles.push_back(p);
+                }
+            }
+        }
+    }
+}
+
+void Engine::update(float dtAsSeconds)
+{
+    auto it = m_particles.begin();
+    while (it != m_particles.end())
+    {
+        if (it -> getTTL() > 0.0)
+        {
+            it -> update(dtAsSeconds);
+            it++;
+        }
+        else
+        {
+            it = m_particles.erase(it);
+        }
+    }
+}
+
+void Engine::draw()
+{
+    m_Window.clear();
+    for (int i = 0; i < m_particles.size(); i++)
+    {
+        m_Window.draw(m_particles[i]);
+        m_Window.display();
+    }
+}
